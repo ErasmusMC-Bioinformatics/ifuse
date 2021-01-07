e61f755eee797be2e366aa6fcb115a2c65ae6126
Saskia Hiltemann <zazkia@gmail.com>
2021-01-07 18:10:36 +0100

    fix reference import

--- ifuse-code/application/libraries/sequenceloader.php
+++ ifuse-code/application/libraries/sequenceloader.php
@@ -8,10 +8,7 @@
 
       public function __construct() {
           $this->CI =& get_instance();
-
-          #$tmp = array_filter(explode(";",$this->CI->userfiles->getFile(null,'OPTIONS')));
-          #while($temp = each($tmp)) {list($key, $value) = explode(":", $temp[1]);$this->opt[strtolower($key)] = $value;}
-      }
+          $this->CI->load->library('userfiles')
 
       public function __destruct() {}
 
@@ -25,6 +22,13 @@
           if (is_array($dataset) && (sizeof($dataset) > 0) && !isset($dataset[0]['Gene.Left.Sequence']) ) {
               $currentFile = $this->CI->userfiles->getFile().".seq";
               $sequences =& $this->sequences;;
+              $tmp = array_filter(explode(";",$this->CI->userfiles->getFile($currentFile,'OPTIONS')));
+              while($temp = each($tmp)) {
+                list($key, $value) = explode(":", $temp[1]);
+                $this->opt[strtolower($key)] = $value;
+              }
+
+
 
               // open genomic reference files
               $seqDir = "{$this->CI->userfiles->basepath}/../R/{$this->opt['reference']}/";
